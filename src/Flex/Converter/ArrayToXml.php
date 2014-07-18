cd45c7b1eb4e756e8220e8278b93da9fad0ed1da
elnebuloso <jan.thoennessen@googlemail.com>
2014-07-19 01:39:27 +0200

    release

---
+++ src/Flex/Converter/ArrayToXml.php
@@ -0,0 +1,213 @@
+<?php
+namespace Flex\Converter;
+
+use DomDocument;
+use Exception;
+
+/**
+ * Class ArrayToXml
+ *
+ * @package Flex\Convert
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ * @info inspired by
+ * @link http://www.lalit.org/lab/convert-php-array-to-xml-with-attributes/
+ */
+class ArrayToXml {
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    /**
+     * @var string
+     */
+    private $encoding;
+
+    /**
+     * @var boolean
+     */
+    private $formatOutput;
+
+    /**
+     * @var DomDocument
+     */
+    private $xml;
+
+    /**
+     * @param string $version
+     * @param string $encoding
+     * @param bool $formatOutput
+     */
+    public function __construct($version = '1.0', $encoding = 'UTF-8', $formatOutput = true) {
+        $this->version = $version;
+        $this->encoding = $encoding;
+        $this->formatOutput = $formatOutput;
+    }
+
+    /**
+     * @param string $version
+     */
+    public function setVersion($version) {
+        $this->version = $version;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion() {
+        return $this->version;
+    }
+
+    /**
+     * @param string $encoding
+     */
+    public function setEncoding($encoding) {
+        $this->encoding = $encoding;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEncoding() {
+        return $this->encoding;
+    }
+
+    /**
+     * @param boolean $formatOutput
+     */
+    public function setFormatOutput($formatOutput) {
+        $this->formatOutput = $formatOutput;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getFormatOutput() {
+        return $this->formatOutput;
+    }
+
+    /**
+     * @param \DomDocument $xml
+     */
+    public function setXml($xml) {
+        $this->xml = $xml;
+    }
+
+    /**
+     * @return \DomDocument
+     */
+    public function getXml() {
+        return $this->xml;
+    }
+
+    /**
+     * @param string $nodeName
+     * @param array $array
+     * @return string
+     */
+    public function convert($nodeName, array $array = array()) {
+        $this->xml = new DomDocument($this->version, $this->encoding);
+        $this->xml->formatOutput = $this->formatOutput;
+        $this->xml->appendChild($this->createXML($nodeName, $array));
+
+        return $this->xml->saveXML();
+    }
+
+    /**
+     * @param string $nodeName
+     * @param array $array
+     * @throws Exception
+     * @return \DOMNode
+     */
+    private function createXML($nodeName, $array) {
+        $node = $this->xml->createElement($nodeName);
+
+        if(is_array($array)) {
+            if(isset($array['@attributes'])) {
+                foreach($array['@attributes'] as $key => $value) {
+                    $this->testTagName($key, $nodeName);
+                    $node->setAttribute($key, $this->createNodeContent($value));
+                }
+
+                unset($array['@attributes']);
+            }
+
+            // value, values cannot have child nodes, so set data and return node
+            if(isset($array['@value'])) {
+                $node->appendChild($this->xml->createTextNode($this->createNodeContent($array['@value'])));
+                unset($array['@value']);
+
+                return $node;
+            }
+
+            // cdata, cdata cannot have child nodes, so set data and return node
+            else if(isset($array['@cdata'])) {
+                $node->appendChild($this->xml->createCDATASection($this->createNodeContent($array['@cdata'])));
+                unset($array['@cdata']);
+
+                return $node;
+            }
+        }
+
+        // possible subnodes
+        if(is_array($array)) {
+            foreach($array as $key => $value) {
+                $this->testTagName($key, $nodeName);
+
+                if(is_array($value) && is_numeric(key($value))) {
+                    foreach($value as $v) {
+                        $node->appendChild($this->createXML($key, $v));
+                    }
+                }
+                else {
+                    $node->appendChild($this->createXML($key, $value));
+                }
+
+                unset($array[$key]);
+            }
+        }
+
+        if(!is_array($array)) {
+            $node->appendChild($this->xml->createTextNode($this->createNodeContent($array)));
+        }
+
+        return $node;
+    }
+
+    /**
+     * @param string $var
+     * @return string
+     */
+    private function createNodeContent($var) {
+        if(is_bool($var)) {
+            return ($var) ? 'true' : 'false';
+        }
+
+        return $var;
+    }
+
+    /**
+     * Check if the tag name or attribute name contains illegal characters
+     *
+     * @link http://www.w3.org/TR/xml/#sec-common-syn
+     * @param string $tag
+     * @return bool
+     */
+    private function isValidTagName($tag) {
+        $pattern = '/^[a-z_]+[a-z0-9\:\-\.\_]*[^:]*$/i';
+
+        return preg_match($pattern, $tag, $matches) && $matches[0] == $tag;
+    }
+
+    /**
+     * @param string $tag
+     * @param string $nodeName
+     * @throws \Exception
+     */
+    private function testTagName($tag, $nodeName) {
+        if(!$this->isValidTagName($tag)) {
+            throw new Exception('illegal characters: ' . $tag . ' in node: ' . $nodeName);
+        }
+    }
+}
---
+++ tests/src/FlexTest/Converter/ArrayToXmlTest.php
@@ -0,0 +1,114 @@
+<?php
+namespace FlexTest\Convert;
+
+use Flex\Converter\ArrayToXml;
+
+/**
+ * Class ArrayToXmlTest
+ *
+ * @package FlexTest\Convert
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ */
+class ArrayToXmlTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @return void
+     */
+    public function test_create_simple() {
+        $expected = '<?xml version="1.0" encoding="UTF-8"?><books><foo>1</foo><bar>2</bar><baz>3</baz></books>';
+        $books = array(
+            'foo' => 1,
+            'bar' => 2,
+            'baz' => 3
+        );
+
+        $converter = new ArrayToXml();
+        $result = $this->filter($converter->convert('books', $books));
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_create_forCollection() {
+        $expected = '<?xml version="1.0" encoding="UTF-8"?><books><element>foo</element><element>bar</element><element>baz</element></books>';
+        $books = array(
+            'element' => array(
+                'foo',
+                'bar',
+                'baz'
+            )
+        );
+
+        $converter = new ArrayToXml();
+        $result = $this->filter($converter->convert('books', $books));
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_create_full() {
+        $expected = '<?xml version="1.0" encoding="UTF-8"?><books type="fiction"><book author="George Orwell"><title>1984</title></book><book author="Isaac Asimov"><title><![CDATA[Foundation]]></title><price>$15.61</price></book><book author="Robert A Heinlein"><title><![CDATA[Stranger in a Strange Land]]></title><price discount="10%">$18.00</price></book></books>';
+        $books = array(
+            '@attributes' => array(
+                'type' => 'fiction'
+            ),
+            'book' => array(
+                array(
+                    '@attributes' => array(
+                        'author' => 'George Orwell'
+                    ),
+                    'title' => '1984'
+                ),
+                array(
+                    '@attributes' => array(
+                        'author' => 'Isaac Asimov'
+                    ),
+                    'title' => array(
+                        '@cdata' => 'Foundation'
+                    ),
+                    'price' => '$15.61'
+                ),
+                array(
+                    '@attributes' => array(
+                        'author' => 'Robert A Heinlein'
+                    ),
+                    'title' => array(
+                        '@cdata' => 'Stranger in a Strange Land'
+                    ),
+                    'price' => array(
+                        '@attributes' => array(
+                            'discount' => '10%'
+                        ),
+                        '@value' => '$18.00'
+                    )
+                )
+            )
+        );
+
+        $converter = new ArrayToXml();
+        $result = $this->filter($converter->convert('books', $books));
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    private function filter($string) {
+        $string = preg_replace(array(
+            '/ {2,}/',
+            '/<!--.*?-->|\t|(?:\r?\n[ \t]*)+/s',
+            '/>\s*/',
+            '/\s*</'
+        ), array(
+            ' ',
+            '',
+            '>',
+            '<'
+        ), $string);
+
+        return $string;
+    }
+}
